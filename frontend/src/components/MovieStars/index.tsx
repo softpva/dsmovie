80e59bb84e2a45f3ca341c6a601fe026d8fb3d41
Pedro Vitor Abreu <soft.eng@gmail.com>
2022-05-12 15:45:24 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as StarFull} from 'assets/img/fullStar.svg';
+import {ReactComponent as StarHalf} from 'assets/img/halfStar.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/emptyStar.svg';
+import './styles.css';
+
+function MovieStars() {
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+
+    )
+};
+
+export default MovieStars;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,42 @@
+import MovieCard from "components/MovieCard";
+// import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
 
-function Listing(){
+function Listing() {
 
-    return(
-        <Pagination/>
+    return (
+        <>
+            <Pagination />
+            {/* Using Bootstrap's breakpoints and classes container, row, col 
+                col-sm-6 represents, by example, if sm < 576px the container will ocupy 6 
+                of 12 Boostrap's total columns
+                mb-3 : margin botton = 3*/
+            }
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-5">
+                        <MovieCard />
+                    </div>
+                    
+                   
+                    
+                </div>
+
+            </div>
+
+        </>
     )
 }
 
